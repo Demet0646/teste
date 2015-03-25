1d42509abc6aa31ab742ea06e8efd3faa06d31b7
Demet0646 <dagilan.demet@gmail.com>
2015-03-25 21:18:12 +0200

    deneme
    rastgele

---
+++ 1/form/deneme/ornek.php
@@ -0,0 +1,25 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form id="form1" name="form1" method="POST" action="ornek1yaz.php">
+<table width="490" height="88" border="0">
+  <tr>
+    <td width="78">sayı 1</td>
+    <td width="15">:</td>
+    <td width="375"><input type="text" name="sayi1" /></td>
+  </tr>
+  <tr>
+    <td>sayı 2</td>
+    <td>:</td>
+    <td><input type="text" name="sayi2" /></td>
+  </tr>
+  <tr>
+    <td><select name="hesap" size="1" id="select">
+      <option value="+">topla</option>
+      <option value="-">çıkar</option>
+      <option value="*">çarp</option>
+      <option value="/">böl</option>
+    </select></td>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="hesapla" id="hesapla" value="hesapla" /></td>
+  </tr>
+</table>
+</form>
